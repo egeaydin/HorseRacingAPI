37be2468026c72b244842698a38b962a3729eb9a
Lauren Dey <>
2018-11-09 15:21:48 -0800

    city can be given as url parameter either as id or city name

--- scrappers/url_forms.py
+++ scrappers/url_forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from datetime import datetime
+from .enums import City
+from django.core.exceptions import ValidationError
 
 
 class RaceDayURLQueryForm(forms.Form):
@@ -10,6 +12,7 @@ class RaceDayURLQueryForm(forms.Form):
     year = forms.IntegerField(max_value=9999, min_value=1000, required=True)
     month = forms.IntegerField(max_value=12, min_value=1, required=True)
     day = forms.IntegerField(max_value=31, min_value=1, required=True)
+    city = forms.CharField(max_length=20, required=False)
 
     @property
     def race_date(self):
@@ -17,3 +20,26 @@ class RaceDayURLQueryForm(forms.Form):
             return datetime(self.cleaned_data['year'],
                             self.cleaned_data['month'],
                             self.cleaned_data['day'])
+
+    def clean_city(self):
+        """
+        City can be either ID or the actual name of the city we can easily validate it by trying to convert the value
+        to enum's value or name. If enum is successfully generated then city is valid.
+        :return:
+        """
+        city = self.cleaned_data['city']
+
+        if city:
+            # first check if incoming parameter is id
+            try:
+                city = City(int(city))
+            except ValueError:
+                # it is not id, now we check for the actual name
+                # we are going to try to create the enum from the key
+                try:
+                    city = City[city]
+                except KeyError:
+                    # it is neither a valid id or a key, so we raise an error
+                    raise ValidationError("Invalid value for City, City should be either valid city id or key",
+                                          code='invalid')
+            return city
--- scrappers/views.py
+++ scrappers/views.py
@@ -12,17 +12,18 @@ class FixtureView(APIView):
         query_params = RaceDayURLQueryForm(self.request.query_params)
         if query_params.is_valid():
             date = query_params.race_date
-            cache_key = City.Izmir.value + date.timestamp()
+            city = query_params.cleaned_data['city']
+            cache_key = city.value + date.timestamp()
 
             # see if it is already cached
             fix = cache.get(cache_key)
             # if not cached
             if not fix:
-                fix = cache.get_or_set(cache_key, FixtureScrapper.scrap_by_date(City.Izmir, date))
+                fix = cache.get_or_set(cache_key, FixtureScrapper.scrap_by_date(city, date))
 
             return Response(self.fixture_to_race_day(fix))
         else:
-            return Response('Invalid or missing parameters!')
+            return Response(query_params.errors)
 
     def fixture_to_race_day(self, fix):
         race_day_dict = {}
