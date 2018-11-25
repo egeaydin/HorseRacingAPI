dfb95c168cd9d2970100b254c9da0618d244594e
Lauren Dey <>
2018-11-25 03:55:06 -0800

    admin added, postgresql added

--- main/settings.py
+++ main/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'rest_framework',
     'main',
-    'gatherer'
+    'gatherer',
+    'django_celery_results'
 ]
 
 REST_FRAMEWORK = {
@@ -84,8 +85,12 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST'),
+        'PORT': config('DATABASE_PORT'),
     }
 }
 
