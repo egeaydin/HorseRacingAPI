fe795cb10ae7c660fca8ab5930bebadaedc4356d
Lauren Dey <>
2018-11-25 12:22:21 -0800

    fixing statics

--- main/settings.py
+++ main/settings.py
@@ -135,10 +135,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+'''
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-)
+)'''
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
